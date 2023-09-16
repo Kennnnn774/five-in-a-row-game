da26987f83e478ccd2ba356cc971dd51cb03ee29
Qianqi Huang <71415495+QianqiHuang@users.noreply.github.com>
2023-09-15 21:28:24 -0400

    update

--- public/client.js
+++ public/client.js
@@ -1,3 +1,4 @@
+
 const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
 const cellSize = canvas.width / 19; // Adjusted to fill the canvas width
@@ -5,44 +6,47 @@ const gridSize = 19;
 let board = Array(gridSize).fill().map(() => Array(gridSize).fill(null));
 let currentPlayer = 'black';
 let myRole = null;
+let playerId = null;
 
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('http://localhost:3000'); // Replace with your server URL
 
-canvas.addEventListener('mousedown', function(e) {
-    if (myRole !== currentPlayer) return; // Ensure the player can only move on their turn
-
-    let x = Math.round((e.offsetX - cellSize / 2) / cellSize);
-    let y = Math.round((e.offsetY - cellSize / 2) / cellSize);
-
-    if (x >= 0 && x < gridSize && y >= 0 && y < gridSize && !board[y][x]) {
-        board[y][x] = currentPlayer;
-        drawBoard();
-    
-        socket.emit('move', { x: x, y: y, player: currentPlayer });
-
-        currentPlayer = currentPlayer === 'black' ? 'white' : 'black';
-        updatePlayerDisplay();  
-    }
-});
-
-const resetButton = document.getElementById('resetButton');
-resetButton.addEventListener('click', resetGame);
-
-let playerId = localStorage.getItem('playerId');
+playerId = sessionStorage.getItem('playerId');
+console.log('session saved', playerId);
 if (playerId) {
-    socket.emit('checkRole', playerId);
+    console.log('client exist', playerId);
+    socket.emit('getOldPlayer', playerId, (response) => {
+        playerId = response.socketId;
+        myRole = response.role;
+        updateRoleDisplay(myRole);        
+    }    );
+} else {
+    socket.emit('createNewPlayer', (response) => {
+        // Callback function to handle the new player ID
+        sessionStorage.setItem('playerId', response.socketId);
+        playerId = sessionStorage.getItem('playerId');
+        myRole = response.role;
+        updateRoleDisplay(myRole);
+        console.log('session saved check', playerId);
+    });    
 }
 
-socket.on('setRole', (role) => {
-    // Set the role for this client and store their ID if not already stored
-    currentPlayer = role;
-    if (!playerId) {
-        playerId = socket.id;
-        localStorage.setItem('playerId', playerId);
-    }
+// if( playerId || myRole === 'observer'){
+//     // need get the saved board information
+//     socket.emit('getBoard', (response) => {
+//         board = response.board;
+//         drawBoard();
+//         console.log('here', board);
+//     })
+// };
+
+socket.on('board', (data) =>{
+    board = data;
+    drawBoard();
+});
+socket.on('currentPlayer', (data) =>{
+    currentPlayer = data;
     updatePlayerDisplay();
 });
-
 // When receiving a move from the server:
 socket.on('move', function(data) {
     board[data.y][data.x] = data.player;
@@ -51,39 +55,43 @@ socket.on('move', function(data) {
     updatePlayerDisplay();
 });
 
-socket.on('player_status', function(data) {
-    if (data.status === 'watch') {
-        // For the sake of simplicity, we'll just display an alert
-        alert("You're watching the game. Wait for your turn to play.");
-    } else if (data.status === 'play') {
-        currentPlayer = data.color;
+socket.on('gameWon', (role) => {
+    alert(`${role} wins the game!`);
+    resetGame(); // Reset the game
+
+});
+
+socket.on('gameReset', () => {
+    alert(`game is reset!`);
+    resetGame(); // Reset the game
+});
+
+canvas.addEventListener('mousedown', function (e) {
+    if (myRole && myRole !== currentPlayer) return;
+
+    let x = Math.round((e.offsetX - cellSize / 2) / cellSize);
+    let y = Math.round((e.offsetY - cellSize / 2) / cellSize);
+
+    if (x >= 0 && x < gridSize && y >= 0 && y < gridSize && !board[y][x]) {
+        board[y][x] = currentPlayer;
+        socket.emit('move', { x: x, y: y, player: currentPlayer });
+        drawBoard();
+        currentPlayer = currentPlayer === 'black' ? 'white' : 'black';
         updatePlayerDisplay();
     }
 });
 
-socket.on('update_turn', function(newPlayer) {
-    currentPlayer = newPlayer;
-    updatePlayerDisplay();
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', function (e) {
+    resetGame();
+    socket.emit('resetGame');
 });
 
-socket.on('role', function(role) {
-    myRole = role;
-    const roleElement = document.getElementById('roleStone');
-
-    // Update the role display based on the role assigned by the server
-    if (role === 'observer') {
-        roleElement.textContent = "Observer";
-        roleElement.style.backgroundColor = 'transparent';
-    } else {
-        roleElement.textContent = "";
-        roleElement.style.backgroundColor = role;
-    }
-});
 
 
 function drawBoard() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     // Drawing the grid
     for (let i = 0; i <= gridSize; i++) {
         ctx.beginPath();
@@ -118,22 +126,25 @@ function updatePlayerDisplay() {
     currentStoneEl.style.backgroundColor = currentPlayer;
 }
 
-function resetGame() {
-    board = Array(19).fill().map(() => Array(19).fill(null));
-    currentPlayer = 'black';
-    drawBoard();
-    updatePlayerDisplay(); 
+function updateRoleDisplay(role){
+    const roleElement = document.getElementById('roleStone');
+    // Update the role display based on the role assigned by the server
+    if (role === 'observer') {
+        roleElement.textContent = "Observer";
+        roleElement.style.backgroundColor = 'transparent';
+    } else {
+        roleElement.textContent = "";
+        roleElement.style.backgroundColor = role;
+    }
 }
 
+
 function resetGame() {
-    board = Array(19).fill().map(() => Array(19).fill(null));
+    board = Array(gridSize).fill().map(() => Array(gridSize).fill(null));
+    currentPlayer = 'black';
     drawBoard();
+    updatePlayerDisplay();
 }
 
 drawBoard();
 updatePlayerDisplay();
-
-
-
-
-
